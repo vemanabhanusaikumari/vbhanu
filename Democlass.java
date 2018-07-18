795fa8d8b10d23cbab7ca776424e257c45e77091
vemanabhanusaikumari <41281142+vemanabhanusaikumari@users.noreply.github.com>
2018-07-18 16:09:15 +0530

    sample

---
+++ Democlass.java
@@ -0,0 +1,16 @@
+package info.oop;
+public class Democlass
+{
+int a=12;
+public void display()
+{
+System.out.println(a);
+}
+public static void main(String[] args)
+{
+Democlass d=new Democlass();
+d.display();
+System.out.print(d.a);
+}
+}
+
